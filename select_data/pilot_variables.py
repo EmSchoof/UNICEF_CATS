5fc52748896ae971aa48b954e563aef2ba73becf
Emily - Work <emily-work@Emilys-MacBook-Pro.local>
2021-02-26 11:22:24 -0500

    import correct ISO_3166-2 country codes

--- select_data/pilot_variables.py
+++ select_data/pilot_variables.py
@@ -1,8 +1,9 @@
-PILOT_EVENTTYPES = ('1','2','3','4','5','6','7','8','9','10','11',
-                    '12','13','14','15','16','17','18','19','20')
+PILOT_EVENTTYPES = ['1','2','3','4','5','6','7','8','9','10','11',
+                    '12','13','14','15','16','17','18','19','20']
 
-PILOT_IOS2 = ('AG','AN','BG','BR','BF','BD','CA','CA','TZ','CO','CO','ER',
-              'ET','GH','GI','HT','IN','IR','IR','KE','KG','LB','LS','LB',
-              'MD','ML','MR','MO','MM','NP','NE','NG','PA','PS','RW','SE',
-              'SL','SO','SS','SD','SY','TJ','UG','UK','VE','YE','ZW','TH',
-              'BE','GA','ZM','TZ','US','NO','IT','FR','SL','MN','SR','NI')
+PILOT_IOS2 = ['AF','AO','BD','BR','BF','BI','CM','CF','TD','CG','CD','ER',
+              'ET','GH','GN','HT','IN','IR','IQ','KE','KG','LB','LS','LY',
+              'MG','ML','MR','MZ','MM','NP','NE','NG','PK','PS','RW','SN',
+              'SL','SO','SS','SD','SY','TJ','UG','UA','VE','YE','ZW','TH',
+              'BJ','GA','ZM','TZ','US','NO','IT','FR','SB','MN','RS','NI']
+
