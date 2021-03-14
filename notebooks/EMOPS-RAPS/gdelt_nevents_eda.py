f3336d20d72501cae486d12ac9c003529fbeb7de
Emily Schoof <emily.schoof@gmail.com>
2021-03-14 16:13:11 -0500

    mv preprocessing to other file

--- notebooks/EMOPS-RAPS/gdelt_nevents_eda.py
+++ notebooks/EMOPS-RAPS/gdelt_nevents_eda.py
@@ -2,6 +2,7 @@
 # DBTITLE 1,Import PySpark Modules
 from functools import reduce
 from itertools import chain
+from pyspark.sql import DataFrame
 import pyspark.sql.functions as F
 from pyspark.sql.types import *
 from pyspark.sql.window import Window
@@ -27,52 +28,6 @@ preprocessedGDELT.limit(10).toPandas()
 
 # COMMAND ----------
 
-preprocessedGDELT.columns
-
-# COMMAND ----------
-
-# DBTITLE 1,Create Columns for Grouped String Columns with Window Functions
-# EventRootCodeString
-groupedERCS = preprocessedGDELT.select('GLOBALEVENTID', 'EventRootCodeString').groupBy('GLOBALEVENTID', 'EventRootCodeString').count()
-windowERCS = Window.partitionBy('EventRootCodeString').orderBy(F.desc('count'))
-groupedERCS = groupedERCS \
-    .withColumn('order', F.row_number().over(windowERCS))\
-    .where(F.col('order') == 1)
-
-groupedERCS.limit(10).toPandas()
-
-# COMMAND ----------
-
-# QuadClassString
-groupedQCS = preprocessedGDELT.select('GLOBALEVENTID', 'QuadClassString').groupBy('GLOBALEVENTID', 'QuadClassString').count()
-windowQCS = Window.partitionBy('QuadClassString').orderBy(F.desc('count'))
-groupedQCS = groupedQCS \
-    .withColumn('order', F.row_number().over(windowQCS))\
-    .where(F.col('order') == 1)
-
-groupedQCS.limit(10).toPandas()
-
-# COMMAND ----------
-
-# ActionGeo_FullName
-groupedAGFN = preprocessedGDELT.select('GLOBALEVENTID', 'ActionGeo_FullName').groupBy('GLOBALEVENTID', 'ActionGeo_FullName').count()
-windowAGFN = Window.partitionBy('ActionGeo_FullName').orderBy(F.desc('count'))
-groupedAGFN = groupedAGFN \
-    .withColumn('order', F.row_number().over(windowAGFN))\
-    .where(F.col('order') == 1)
-
-groupedAGFN.limit(10).toPandas()
-
-# COMMAND ----------
-
-# DBTITLE 1,Create Target Variable Column
-df.groupBy('x').count().select('x', f.col('count').alias('n')).show()
-
-
-nEventsGDELT = preprocessedGDELT /
-               .groupBy('GLOBALEVENTID') /
-               .agg(
-                 F.mode()
-                 F.avg()
-               )
-preprocessedGDELT.limit(5).toPandas()
+# DBTITLE 1,Count Unique Global Event Ids
+print((preprocessedGDELT.count(), len(preprocessedGDELT.columns)))
+preprocessedGDELT.agg(F.countDistinct(F.col("GLOBALEVENTID")).alias("nEvents")).show()
