34d245b4e5a05a6b8eb44f83c922521d6533d482
Emily Schoof <emily.schoof@gmail.com>
2021-03-15 13:41:38 -0500

    init resolve deprecated codes + nulls

--- notebooks/EMOPS-RAPS/gdelt_data_preprocessing.py
+++ notebooks/EMOPS-RAPS/gdelt_data_preprocessing.py
@@ -1,13 +1,7 @@
 # Databricks notebook source
-# DBTITLE 0,Preliminary: Import Non-Standard Libraries in to Databricks
-# For libraries that are not part of the standard distribution of Databricks, use the dbutils.library methods
-#dbutils.library.installPyPI("pandas")
-#dbutils.library.installPyPI("numpy")
-#dbutils.library.restartPython()
-
-# COMMAND ----------
-
 # DBTITLE 1,Import PySpark Modules
+from functools import reduce
+from itertools import chain
 import pyspark.sql.functions as F
 from pyspark.sql.types import *
 
@@ -22,20 +16,20 @@ bq_gdelt.limit(10).toPandas()
 
 # DBTITLE 1,Define Data Schema
 data_schema = [
-               StructField('GLOBALEVENTID', IntegerType(), True),
-               StructField('EventTimeDate', StringType(), True),
-               StructField('MentionTimeDate', StringType(), True),
+               StructField('GLOBALEVENTID', IntegerType(), True), # numerical
+               StructField('EventTimeDate', LongType(), True), # numerical
+               StructField('MentionTimeDate', LongType(), True), # numerical
                StructField('Confidence', IntegerType(), True),
                StructField('MentionDocTone', FloatType(), True),
                StructField('EventCode', StringType(), True),
                StructField('EventRootCode', StringType(), True),
-               StructField('QuadClass', IntegerType(), True),
-               StructField('GoldsteinScale', FloatType(), True),
+               StructField('QuadClass', IntegerType(), True), # numerical
+               StructField('GoldsteinScale', FloatType(), True), # numerical
                StructField('ActionGeo_Type', StringType(), True),
                StructField('ActionGeo_FullName', StringType(), True),
                StructField('ActionGeo_CountryCode', StringType(), True),
-               StructField('ActionGeo_Lat', FloatType(), True),
-               StructField('ActionGeo_Long', FloatType(), True),
+               StructField('ActionGeo_Lat', FloatType(), True), # numerical
+               StructField('ActionGeo_Long', FloatType(), True), # numerical
                StructField('SOURCEURL', StringType(), True),
             ]
 
@@ -43,93 +37,39 @@ final_struc = StructType(fields = data_schema)
 
 # COMMAND ----------
 
+# DBTITLE 1,Create DataFrame for Manipulation with Defined Schema
 gdeltFeb = sqlContext.createDataFrame(bq_gdelt.rdd, final_struc)
-gdeltFeb.limit(10).toPandas()
+gdeltFeb.limit(5).toPandas()
 
 # COMMAND ----------
 
-# DBTITLE 1,Convert String Date Columns to Datetimes
-# This function converts the string cell into a date:
-func =  F.udf (lambda x: datetime.strptime(x, 'yyyyMMddHHmmss'), DateType())
-gdeltFeb1 = gdeltFeb.withColumn('EventTimeDate', func(F.col('EventTimeDate')))
-gdeltFebDates = gdeltFeb1.withColumn('MentionTimeDate', func(F.col('MentionTimeDate')))
-gdeltFebDates.printSchema()
+# DBTITLE 1,Assess DataFrame Distributions in Target Variables
+gdeltFeb.select("GLOBALEVENTID","EventTimeDate","MentionTimeDate", "Confidence", "MentionDocTone", "EventCode", "EventRootCode", "QuadClass", "GoldsteinScale").describe().show()
 
 # COMMAND ----------
 
-# DBTITLE 1,Assess GDELT Data Distributions
-gdeltFeb.select("GLOBALEVENTID","EventTimeDate","MentionTimeDate", "Confidence", "MentionDocTone", "EventRootCode", "QuadClass", "GoldsteinScale").describe().show()
+# DBTITLE 1,Replace Obvious, Irregular Values with Nulls
+# Event Code
+gdeltFeb = gdeltFeb.withColumn(
+    'EventCode',
+    F.when(
+        F.col('EventCode').isin('---', '--'),
+        None
+    ).otherwise(F.col('EventCode')).cast('int')
+)
 
-# COMMAND ----------
+# Event Root Code
+gdeltFeb = gdeltFeb.withColumn(
+    'EventRootCode',
+    F.when(
+        F.col('EventRootCode').isin('---', '--'),
+        None
+    ).otherwise(F.col('EventRootCode')).cast('int')
+)
 
-# DBTITLE 1,Assess Null Values
-result2 = spark.sql(
-  """
-  SELECT 
-    education,
-    ROUND( AVG( if(LTRIM(marital_status) = 'Never-married',1,0) ), 2) as bachelor_rate
-  FROM 
-    adult 
-  GROUP BY education
-  ORDER BY bachelor_rate DESC
-  """)
-
-# register the df we just made as a table for spark sql named a table -> "result2" 
-sqlContext.registerDataFrameAsTable(result2, "result2")
-spark.sql("SELECT * FROM result2").show(1)
-
-
-
-
-
-gdeltFebNulls = gdeltFeb.filter(gdeltFeb.GLOBALEVENTID.isNull() | gdeltFeb.EventTimeDate.isNull() | gdeltFeb.MentionTimeDate.isNull() | 
-                                gdeltFeb.Confidence.isNull() | gdeltFeb.MentionDocTone.isNull() | gdeltFeb.EventRootCode.isNull() | 
-                                gdeltFeb.QuadClass.isNull() | gdeltFeb.GoldsteinScale.isNull() )
-gdeltFebNulls.describe()
-
-# COMMAND ----------
-
-# DBTITLE 1,Assess Null Values
-gdeltFebNulls = gdeltFeb.na.drop(subset=["GLOBALEVENTID","EventTimeDate","MentionTimeDate", "Confidence", "MentionDocTone", "EventRootCode", "QuadClass", "GoldsteinScale"])
-
-# COMMAND ----------
-
-# DBTITLE 1,Define Reusable Python Functions
-def get_code_strings(df, codes_col: str, strings: list):
-    """
-    Create Column for Strings Associated with Code Column
-    
-    :param df: dataframe of cleaned data
-    :param codes_col: name of the code column in dataframe
-    :param strings: list of strings associated with code column
-    :rtype: dataframes
-    :return: 
-        :df: param dataframe with new string column
-        :verified_df: dataframe to confirm correct code/string column creation
-    """
-    
-    assert len(df[codes_col].unique()) == len(strings), "Length of codes and strings list are not equal"
-    
-    # Convert lists to dictionary 
-    codes = df[codes_col].sort_values(ascending=True).unique()
-    code_dict = {codes[i]: strings[i] for i in range(len(codes))}
-    
-    # Add column for code strings
-    codes_string_col = codes_col+'String'
-    df[codes_string_col] = df[codes_col].map(code_dict)
-
-    # verify output
-    verify_df = df[[codes_col, codes_string_col]].sort_values(by=codes_col, ascending=True).drop_duplicates()
-
-    # return df and verified output
-    return df, verify_df
-
-# COMMAND ----------
-
-# DBTITLE 1,Select Mentions within first 60 Days of an Event
-# Calculate days between
-#bq_gdeltFebDaysBetween = bq_gdelt.withColumn('DaysBetween', bq_gdelt.select(F.col("MentionTimeDate")-F.col("EventTimeDate")))
-bq_gdelt.select(F.col("MentionTimeDate")-F.col("EventTimeDate"))
+# Verify Output
+print(gdeltFeb.dtypes)
+gdeltFeb.select("EventCode", "EventRootCode").describe().show()
 
 # COMMAND ----------
 
@@ -146,18 +86,91 @@ bq_gdelt.select(F.col("MentionTimeDate")-F.col("EventTimeDate"))
 
 # COMMAND ----------
 
-# DBTITLE 0,Select Data Associated with Target Country Code List
-# Drop all rows in merged_df with nulls in the specified columns
-required_value_columns = ['GLOBALEVENTID', 'EventTimeDate', 'ActionGeo_CountryCode', 
-                          'EventCode', 'GoldsteinScale', 'MentionDocTone']
+# DBTITLE 1,Assess Null Values
+def count_missings(spark_df, sort=True):
+    """
+    Counts number of nulls and nans in each column
+    """
+    df = spark_df.select([F.count(F.when(F.isnan(c) | F.isnull(c), c)).alias(c) for (c,c_type) in spark_df.dtypes if c_type not in ('timestamp', 'string', 'date')]).toPandas()
 
-cleaned_merged_df = merged_df[~pd.isnull(merged_df[required_value_columns]).any(axis=1)].reset_index(drop=True)
-print(cleaned_merged_df.shape)
-cleaned_merged_df.head(1)
+    if len(df) == 0:
+        print("There are no any missing values!")
+        return None
+
+    if sort:
+        return df.rename(index={0: 'count'}).T.sort_values("count",ascending=False)
+
+    return df
+
+# COMMAND ----------
+
+# Non String Values
+count_missings(gdeltFeb)
+
+# COMMAND ----------
+
+# DBTITLE 1,DATA REMOVAL (1):  Drop Rows with Nulls in Key Columns (add all in list as a precaution)
+print('Original Dataframe: ', (gdeltFeb.count(), len(gdeltFeb.columns)))
+
+# drop Nulls in Integer Columns
+gdeltFebNoNulls1 = gdeltFeb.na.drop(subset=["GLOBALEVENTID","EventTimeDate","MentionTimeDate", "ActionGeo_CountryCode", "Confidence", "MentionDocTone", "EventRootCode", "QuadClass", "GoldsteinScale"])
+
+# drop Nulls in string columns
+gdeltFebNoNulls = gdeltFebNoNulls1.where(F.col('ActionGeo_CountryCode').isNotNull())
+
+# verify output
+print('Removal of Nulls Dataframe: ', (gdeltFebNoNulls.count(), len(gdeltFebNoNulls.columns)))
+count_missings(gdeltFebNoNulls)
+
+# COMMAND ----------
+
+gdeltFebNoNulls.printSchema()
+gdeltFebNoNulls.limit(5).toPandas()
+
+# COMMAND ----------
+
+# DBTITLE 1,Convert Integer Date Columns to Strings then to DateTimes
+# Convert date-int columns to string columns
+gdeltFebNoNulls = gdeltFebNoNulls.withColumn('EventTimeDate', F.expr("CAST(EventTimeDate AS STRING)"))
+gdeltFebNoNulls = gdeltFebNoNulls.withColumn('MentionTimeDate', F.expr("CAST(MentionTimeDate AS STRING)"))
+
+# Convert date-strings to date columns
+gdeltFebNoNulls = gdeltFebNoNulls.withColumn('EventTimeDate', F.to_date(F.unix_timestamp(F.col('EventTimeDate'), 'yyyyMMddHHmmss').cast("timestamp")))
+gdeltFebNoNulls = gdeltFebNoNulls.withColumn('MentionTimeDate', F.to_date(F.unix_timestamp(F.col('MentionTimeDate'), 'yyyyMMddHHmmss').cast("timestamp")))
+
+# Confirm output
+gdeltFebNoNulls.printSchema()
+gdeltFebNoNulls.limit(2).toPandas()
+
+# COMMAND ----------
+
+# DBTITLE 1,Calculate Days Between Mentions and Events Data
+gdeltFebNoNulls = gdeltFebNoNulls.withColumn('DaysBetween', F.datediff(F.col('MentionTimeDate'),F.col('EventTimeDate')).cast('int'))
+gdeltFebNoNulls.printSchema()
+gdeltFebNoNulls.limit(2).toPandas()
+
+# COMMAND ----------
+
+# DBTITLE 1,DATA REMOVAL (2): Select Mentions within first 15 Days of an Event
+print('Removal of Nulls Dataframe: ', (gdeltFebNoNulls.count(), len(gdeltFebNoNulls.columns)))
+
+# Select Data Based on DaysBetween Column
+gdeltFebNoNullsSelectD = gdeltFebNoNulls.where(F.col('DaysBetween') <= 15)
+
+# Confirm output
+print('Mentions within 15days of Event Dataframe: ', (gdeltFebNoNullsSelectD.count(), len(gdeltFebNoNullsSelectD.columns)))
+
+# COMMAND ----------
+
+# DBTITLE 1,Assess Remaining Event Dates
+datesDF = gdeltFebNoNullsSelectD.select('EventTimeDate')
+min_date, max_date = datesDF.select(F.min('EventTimeDate'),F.max('EventTimeDate')).first()
+min_date, max_date
 
 # COMMAND ----------
 
 # DBTITLE 1,Create Cameo Code Root Integer Values with Associated String
+# Create CAMEO verbs list
 cameo_verbs = ['MAKE PUBLIC STATEMENT','APPEAL','EXPRESS INTENT TO COOPERATE','CONSULT',
               'ENGAGE IN DIPLOMATIC COOPERATION','ENGAGE IN MATERIAL COOPERATION','PROVIDE AID',
                'YIELD','INVESTIGATE','DEMAND','DISAPPROVE','REJECT','THREATEN','PROTEST',
@@ -165,56 +178,183 @@ cameo_verbs = ['MAKE PUBLIC STATEMENT','APPEAL','EXPRESS INTENT TO COOPERATE','C
                'ENGAGE IN UNCONVENTIONAL MASS VIOLENCE']
 print(cameo_verbs)
 
-# Add string column
-cleaned_merged_df, cameo_code_df = get_code_strings(cleaned_merged_df, 'EventRootCode', cameo_verbs)
-cameo_code_df
+# Create distinct list of CAMEO EventRootCodes
+cameo_codes = gdeltFebNoNullsSelectD.select('EventRootCode').distinct().rdd.map(lambda r: r[0]).collect()
+cameo_codes_ordered = sorted(cameo_codes)
+print(cameo_codes_ordered)
+
+# Create CAMEO EventRootCodes: CAMEO verbs dictionary
+cameo_verbs_dict = dict(zip(cameo_codes_ordered, cameo_verbs))
+cameo_verbs_dict
 
 # COMMAND ----------
 
-# verify output
-cleaned_merged_df.head(2)
+# Map dictionary over df to create string column
+mapping_expr = F.create_map([F.lit(x) for x in chain(*cameo_verbs_dict.items())])
+gdeltFebNoNullsSelectD = gdeltFebNoNullsSelectD.withColumn('EventRootCodeString', mapping_expr[F.col('EventRootCode')])
+
+# Confirm accurate output
+gdeltFebNoNullsSelectD.select('EventRootCode', 'EventRootCodeString').dropDuplicates().sort(F.col('EventRootCode')).show()
+gdeltFebNoNullsSelectD.limit(1).toPandas()
 
 # COMMAND ----------
 
 # DBTITLE 1,Create Cameo QuadClass Integer Values with Associated String
+# Create CAMEO QuadClass String List
 cameo_quadclass = ['Verbal Cooperation','Material Cooperation','Verbal Conflict','Material Conflict']
 print(cameo_quadclass)
 
-# get string column
-cleaned_merged_df, cameo_quadclass_df = get_code_strings(cleaned_merged_df, 'QuadClass', cameo_quadclass)
-cameo_quadclass_df
+# Create distinct list of CAMEO QuadClass codes
+cameo_quadclass_codes = gdeltFebNoNullsSelectD.select('QuadClass').distinct().rdd.map(lambda r: r[0]).collect()
+cameo_quadclass_codes_ordered = sorted(cameo_quadclass_codes)
+print(cameo_quadclass_codes_ordered)
+
+# Create CAMEO QuadClass: CAMEO QuadClass String dictionary
+cameo_quadclass_dict = dict(zip(cameo_quadclass_codes_ordered, cameo_quadclass))
+cameo_quadclass_dict
 
 # COMMAND ----------
 
-# DBTITLE 0,Start Visualizing Data
-# verify output
-cleaned_merged_df.head(2)
+# Map dictionary over df to create string column
+mapping_expr = F.create_map([F.lit(x) for x in chain(*cameo_quadclass_dict.items())])
+gdeltFebNoNullsSelectD = gdeltFebNoNullsSelectD.withColumn('QuadClassString', mapping_expr[F.col('QuadClass')])
+
+# Confirm accurate output
+gdeltFebNoNullsSelectD.select('QuadClass', 'QuadClassString').dropDuplicates().sort(F.col('QuadClass')).show()
+gdeltFebNoNullsSelectD.limit(1).toPandas()
 
 # COMMAND ----------
 
 # DBTITLE 1,Create Country Code Integer Values with Country Names
-# get country data
-country_codes_df = pd.DataFrame(pd.read_csv('../select_data/data/countries.csv', encoding= 'unicode_escape'))
+# CSV options
+infer_schema = "true"
+first_row_is_header = "true"
+delimiter = ","
 
-# convert lists to dictionary 
-country_code_dict = {country_codes_df['alpha-2'][i]: country_codes_df['name'][i] for i in range(len(country_codes_df))}
+# The applied options are for CSV files.  
+country_codes_df = spark.read.format("csv") \
+  .option("inferSchema", infer_schema) \
+  .option("header", first_row_is_header) \
+  .option("sep", delimiter) \
+  .load("/Filestore/tables/countries.csv")
 
-# Add column for cameo code root strings (verbs)
-cleaned_merged_df['ActionGeo_FullName'] = cleaned_merged_df['ActionGeo_CountryCode'].map(country_code_dict)
-cleaned_merged_df.head()
+# Replace \x81 with an empty string
+#country_codes_df = country_codes_df.withColumn('name', F.regexp_replace('name', '\x81Aland Islands', 'Aland Islands'))  
+country_codes_df.limit(5).toPandas()
 
 # COMMAND ----------
 
-# DBTITLE 0,Plot Incidences by Country
+# Create Country Name and Country FIPS 10-4 Code Lists
+country_names = country_codes_df.select('Country name').rdd.flatMap(lambda x: x).collect()
+country_fips104 = country_codes_df.select('FIPS 10-4').rdd.flatMap(lambda x: x).collect()
+
+# Create 2-Digit Country Code: Country Name dictionary
+country_fips104_dict = dict(zip(country_fips104, country_names))
+country_fips104_dict
+
+# COMMAND ----------
+
+# Map dictionary over df to create string column
+mapping_expr = F.create_map([F.lit(x) for x in chain(*country_fips104_dict.items())])
+gdeltFebNoNullsSelectD = gdeltFebNoNullsSelectD.withColumn('ActionGeo_FullName', mapping_expr[F.col('ActionGeo_CountryCode')])
+
+# Confirm accurate output
+print(gdeltFebNoNullsSelectD.select('ActionGeo_CountryCode', 'ActionGeo_FullName').dropDuplicates().sort(F.col('ActionGeo_FullName')).count())
+gdeltFebNoNullsSelectD.limit(1).toPandas()
+
+# COMMAND ----------
+
+# Assess Nulls in Country Name Strings
+nullCountries = gdeltFebNoNullsSelectD.select('ActionGeo_CountryCode', 'ActionGeo_FullName').dropDuplicates().sort(F.col('ActionGeo_FullName')).where(F.col('ActionGeo_FullName').isNull()).show()
+print(nullCountries.count())
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC With the incidence of the country code, 'YI', it appears that GDELT uses the *depreciated* version of FIPS 10-4
+# MAGIC 
+# MAGIC "YI - Serbia and Montenegro (deprecated FIPS 10-4 country code, now RB (Serbia) and MJ (Montenegro))"
+
+# COMMAND ----------
+
+# DBTITLE 1,Assess Depreciated FIPS 10-4 country codes
+t = gdeltFebNoNullsSelectD.withColumn(
+    'EventCode',
+    F.when(
+        (F.col('ActionGeo_CountryCode') == 'YI',
+        "Serbia and Montenegro"),
+        (F.col('ActionGeo_CountryCode') == 'PF',
+        ""),
+      
+    ).otherwise(F.col('EventCode')).cast('int')
+)
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# DBTITLE 1,Verify NoNulls in Target Variables
+print('Removal of Nulls Dataframe: ', (gdeltFebNoNullsSelectD.count(), len(gdeltFebNoNullsSelectD.columns)))
+count_missings(gdeltFebNoNullsSelectD)
+
+# COMMAND ----------
+
+# DBTITLE 1,Assess Remaining Event Dates
+datesDF = gdeltFebNoNullsSelectD.select('EventTimeDate')
+min_date, max_date = datesDF.select(F.min('EventTimeDate'),F.max('EventTimeDate')).first()
+min_date, max_date
+
+# COMMAND ----------
+
+# DBTITLE 1,Count Unique Global Events
+# select specific columns
+select_columns = ['GLOBALEVENTID',
+                 'EventTimeDate',
+                 'EventRootCodeString',
+                 'QuadClassString',
+                 'MentionTimeDate',
+                 'Confidence',
+                 'MentionDocTone',
+                 'GoldsteinScale',
+                 'ActionGeo_FullName',
+                 'ActionGeo_Lat',
+                 'ActionGeo_Long'
+                 ]
+
+
+preprocessedGDELT = gdeltFebNoNullsSelectD.select(select_columns)
+print((preprocessedGDELT.count(), len(preprocessedGDELT.columns)))
+preprocessedGDELT = preprocessedGDELT.withColumn("nEvents", F.lit(1))
+preprocessedGDELT.agg(F.countDistinct(F.col("GLOBALEVENTID")).alias("nEvents")).show()
+
+# COMMAND ----------
+
+#df.agg(F.min(df.High),F.max(df.High),F.avg(df.High),F.sum(df.High))
+
+# COMMAND ----------
+
+# DBTITLE 1,Create Target Variable Columns
+# select specific columns for output
+gbcolumns = ['GLOBALEVENTID','EventTimeDate','ActionGeo_FullName','QuadClassString','EventRootCodeString','ActionGeo_Lat','ActionGeo_Long']
+
+# group data based on desired output
+groupedCountryEvents = preprocessedGDELT.groupBy(gbcolumns).agg(F.avg('Confidence'),
+                                                                F.avg('MentionDocTone'),
+                                                                F.avg('GoldsteinScale'),
+                                                                F.sum('nEvents')
+)
+
 # verify output
-cameo_country_df = cleaned_merged_df[['ActionGeo_CountryCode', 'ActionGeo_FullName']].sort_values(by='ActionGeo_CountryCode',
-                                                                                ascending=True).drop_duplicates()
-cameo_country_df.head(50)
+groupedCountryEvents.limit(10).toPandas()
 
 # COMMAND ----------
 
-# DBTITLE 1,Verify Cleaned-Data Output
-# Select Desired Columns for Data Factory Output
-cleaned_merged_df = cleaned_merged_df[desired_columns]
-print('Cleaned Data with Desired Columns: ',cleaned_merged_df.shape)
-cleaned_merged_df.head()
+#groupedCountryEvents.limit(10).toPandas()
+
+# COMMAND ----------
+
+# DBTITLE 1,Save DataFrame as CSV
+groupedCountryEvents.write.format('csv').option('header',True).mode('overwrite').option('sep',',').save('/Filestore/tables/tmp/gdelt/preprocessed.csv')
