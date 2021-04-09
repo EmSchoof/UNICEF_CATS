5af7db88f08db953d8a0cda24131062cff30907d
Emily Schoof <emily.schoof@gmail.com>
2021-04-09 09:46:52 -0500

    calc running median

--- notebooks/EMOPS-RAPS/gdelt_5_statistical_model.py
+++ notebooks/EMOPS-RAPS/gdelt_5_statistical_model.py
@@ -12,3 +12,69 @@
 # MAGIC *Execution of Methodology*
 # MAGIC - 1 - [create a list of 3 day moving averages from today - 365 days] // compare this list with defined 'z' anomalist behavior to the current 3 day average per EventRootCode
 # MAGIC - 2 - [create a list of 60 day moving averages from today - 730 days] // compare this list with defined 'z' anomalist behavior to the current 60 day average per EventRootCode
+
+# COMMAND ----------
+
+# DBTITLE 1,Import Modules
+import numpy as np
+import pandas as pd
+from pyspark.mllib.stat import Statistics
+from pyspark.sql import DataFrame
+import pyspark.sql.functions as F
+from pyspark.sql.types import *
+from pyspark.sql.window import Window
+import scipy.stats as stats
+
+# COMMAND ----------
+
+# DBTITLE 1,Import ERV Data
+# CSV options
+infer_schema = "true"
+first_row_is_header = "true"
+delimiter = ","
+
+# The applied options are for CSV files.  
+eventsReportValue = spark.read.format("csv") \
+  .option("inferSchema", infer_schema) \
+  .option("header", first_row_is_header) \
+  .option("sep", delimiter) \
+  .load("/Filestore/tables/tmp/gdelt/targetvalues_confidence40plus.csv") #
+print((eventsReportValue.count(), len(eventsReportValue.columns)))
+eventsReportValue.limit(5).toPandas()
+
+# COMMAND ----------
+
+# DBTITLE 1,Import GRV and TRV Data
+# The applied options are for CSV files.  
+goldToneReportValues = spark.read.format("csv") \
+  .option("inferSchema", infer_schema) \
+  .option("header", first_row_is_header) \
+  .option("sep", delimiter) \
+  .load("/Filestore/tables/tmp/gdelt/gold_tone_targetvalues_confidence40plus.csv") #
+print((goldToneReportValues.count(), len(goldToneReportValues.columns)))
+goldToneReportValues.limit(5).toPandas()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Part 1:
+# MAGIC #### Calculate the running Median of each column [source](https://code.activestate.com/recipes/576930/)
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
