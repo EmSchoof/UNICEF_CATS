3ef6d211c21353e2c8217062ba708d9c6456b7ad
Emily Schoof <emily.schoof@gmail.com>
2021-05-03 09:12:52 -0500

    change nArticles to FATALITIES

--- notebooks/EMOPS-RAPS/acled_data_preprocessing.py
+++ notebooks/EMOPS-RAPS/acled_data_preprocessing.py
@@ -46,9 +46,8 @@ acledSelect.limit(3).toPandas()
 # COMMAND ----------
 
 # Select specific columns
-select_cols = ['COUNTRY','YEAR','EVENT_DATE','EVENT_TYPE','SUB_EVENT_TYPE']
+select_cols = ['COUNTRY','YEAR','EVENT_DATE','EVENT_TYPE','SUB_EVENT_TYPE', 'FATALITIES']
 acledSelectCols = acledSelect.select(select_cols)
-acledSelectCols = acledSelectCols.withColumn('nEvent', F.lit(1))
 print((acledSelectCols.count(), len(acledSelectCols.columns)))
 acledSelectCols.limit(3).toPandas()
 
@@ -60,7 +59,7 @@ data_schema = [
                StructField('EVENT_DATE', StringType(), True),
                StructField('EVENT_TYPE', StringType(), True), 
                StructField('SUB_EVENT_TYPE', StringType(), True),
-               StructField('nEvent', IntegerType(), True),
+               StructField('FATALITIES', IntegerType(), True),
             ]
 
 final_struc = StructType(fields = data_schema)
@@ -74,13 +73,13 @@ acledRDD.limit(5).toPandas()
 # COMMAND ----------
 
 # DBTITLE 1,Assess Event Types and Sub Event Types
-eventTypes = acledRDD.select('EVENT_TYPE').groupBy('EVENT_TYPE').count() #'SUB_EVENT_TYPE'
+eventTypes = acledRDD.select('EVENT_TYPE', 'FATALITIES').groupBy('EVENT_TYPE').sum('FATALITIES') #'SUB_EVENT_TYPE'
 eventTypes.toPandas()
 
 # COMMAND ----------
 
 # DBTITLE 0,Assess Event Types and Sub Event Types
-eventTypes = acledRDD.select('SUB_EVENT_TYPE').groupBy('SUB_EVENT_TYPE').count() #'EVENT_TYPE'
+eventTypes = acledRDD.select('SUB_EVENT_TYPE', 'FATALITIES').groupBy('SUB_EVENT_TYPE').sum('FATALITIES') #'EVENT_TYPE'
 eventTypes.toPandas()
 
 # COMMAND ----------
@@ -112,7 +111,7 @@ acledRDDNoNulls.limit(2).toPandas()
 # COMMAND ----------
 
 # DBTITLE 1,Select Data from 2021
-acled2021 = acledRDDNoNulls.filter(F.col('YEAR') >= 2021)
+acled2021 = acledRDDNoNulls.filter(F.col('YEAR') >= 2021).filter(acledRDDNoNulls['EVENT_DATE'] >= F.lit('2021-03-01'))
 acled2021.limit(2).toPandas()
 
 # COMMAND ----------
@@ -120,7 +119,7 @@ acled2021.limit(2).toPandas()
 # DBTITLE 1,Assess DataFrame Distributions in Target Variables
 # Create New Dataframe Column to Count Number of Daily Articles by Country by EventRootCode
 acledTargetOutput = acled2021.groupBy('COUNTRY','EVENT_DATE','EVENT_TYPE') \
-                              .agg(F.sum('nEvent').alias('nArticles'))
+                              .agg(F.sum('FATALITIES').alias('FATALITIES'))
 acledTargetOutput.limit(5).toPandas()
 
 # COMMAND ----------
@@ -132,7 +131,7 @@ min_date, max_date
 
 # COMMAND ----------
 
-acledPandas = acledTargetOutput.toPandas().groupby(['COUNTRY', 'EVENT_DATE'], as_index=False).agg({'nArticles' : 'sum'})
+acledPandas = acledTargetOutput.toPandas().groupby(['COUNTRY', 'EVENT_DATE'], as_index=False).agg({'FATALITIES' : 'sum'})
 acledPandas.head()
 
 # COMMAND ----------
@@ -148,7 +147,7 @@ import seaborn as sns
 
 fig, ax = plt.subplots(figsize=(16, 24))
 sns.boxplot(data=acledPandas,
-            x="nArticles",
+            x="FATALITIES",
             y="COUNTRY",
             ax=ax)
 
@@ -159,15 +158,9 @@ for country in countries:
   acledPandas.loc[ acledPandas['COUNTRY'] == country ].set_index('EVENT_DATE').plot(ax=ax)
   ax.set(title="Conflict Articles Over Time in " + country,
          xlabel="EVENT_DATE",
-         ylabel="nArticles")
+         ylabel="FATALITIES")
   ax.axhline(0, linestyle="dashed", color="black", alpha=0.5)
   plt.show()
 
 # COMMAND ----------
 
-
-
-# COMMAND ----------
-
-# DBTITLE 1,Save DataFrame as CSV
-outputPreprocessedGDELT.write.format('csv').option('header',True).mode('overwrite').option('sep',',').save('/FileStore/tables/tmp/gdelt/preprocessed.csv')
